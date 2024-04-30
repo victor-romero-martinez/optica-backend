c579d5e5ea4f3038f2a6c5b0c72161c26fdb6810
victor-romero-martinez <victordav147@gmail.com>
2024-04-30 13:16:41 -0400

    release: style route

--- src/styles/styles.controller.ts
+++ src/styles/styles.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { StylesService } from './styles.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Roles } from 'src/roles/roles.decorator';
+import { Role } from 'src/roles/roles.enum';
 import { CreateStyleDto } from './dto/create-style.dto';
 import { UpdateStyleDto } from './dto/update-style.dto';
+import { StylesService } from './styles.service';
 
 @Controller('styles')
 export class StylesController {
   constructor(private readonly stylesService: StylesService) {}
 
+  @Roles(Role.Admin)
   @Post()
   create(@Body() createStyleDto: CreateStyleDto) {
     return this.stylesService.create(createStyleDto);
@@ -22,11 +33,13 @@ export class StylesController {
     return this.stylesService.findOne(+id);
   }
 
+  @Roles(Role.Admin)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateStyleDto: UpdateStyleDto) {
     return this.stylesService.update(+id, updateStyleDto);
   }
 
+  @Roles(Role.Admin)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.stylesService.remove(+id);
--- src/styles/styles.module.ts
+++ src/styles/styles.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { StylesService } from './styles.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Style } from './entities/style.entity';
 import { StylesController } from './styles.controller';
+import { StylesService } from './styles.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Style])],
   controllers: [StylesController],
   providers: [StylesService],
 })
